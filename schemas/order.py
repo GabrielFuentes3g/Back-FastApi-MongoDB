f1280e134879ad954c9f3839da909b0a7065ba6b
Gabriel <afuentes3@ucol.mx>
2025-10-08 11:35:09 -0600

    orderEntity and ordersEntity defined

---
+++ schemas/order.py
@@ -0,0 +1,14 @@
+def orderEntity(item) -> dict:
+    return {
+        "id": str(item["_id"]),
+        "userid": item.get("userid"),
+        "orderDate": item.get("orderDate"),
+        "status": item.get("status"),
+        "totalAmount": item.get("totalAmount"),
+        "items": item.get("items", []),
+        "createdAt": item.get("createdAt"),
+        "updatedAt": item.get("updatedAt")
+    }
+
+def ordersEntity(entity) -> list:
+    return [orderEntity(item) for item in entity]
