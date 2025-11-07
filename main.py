7cbc57c064d32a031a2fcda4a159e1a1d8076bee
Gabriel <afuentes3@ucol.mx>
2025-11-07 10:29:13 -0600

    add category routes for CRUD operations

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from fastapi import FastAPI
 from routes.product import product
 from routes.store import store
 from routes.user import user
+from routes.category import category
 
 main = FastAPI()
 
 main.include_router(product)
 main.include_router(store)
 main.include_router(user)
+main.include_router(category)
