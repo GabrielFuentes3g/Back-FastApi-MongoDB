12a38fa7e977b5ed20c90efd84057781258b63a8
Gabriel <afuentes3@ucol.mx>
2025-11-07 10:51:54 -0600

    add payment routes for CRUD operations

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ from routes.store import store
 from routes.user import user
 from routes.category import category
 from routes.order import order
+from routes.payment import payment
 
 main = FastAPI()
 
 main.include_router(product)
 main.include_router(store)
+main.include_router(payment)
 main.include_router(order)
 main.include_router(user)
 main.include_router(category)
