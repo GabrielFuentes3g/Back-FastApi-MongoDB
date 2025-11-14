e81ff8387f55a39f9aed9d68b562bc85e2c508d4
Gabriel <afuentes3@ucol.mx>
2025-11-14 14:10:37 -0600

    feat: implement order item management routes for creating, retrieving, updating, and deleting order items

--- routes/order.py
+++ routes/order.py
@@ -3,46 +3,17 @@ from unittest import result
 from config.db import db
 from fastapi import APIRouter, HTTPException
 from models.order import Order
-from models.orderItem import OrderItem
 from schemas.order import orderEntity, ordersEntity
-from schemas.orderItem import orderItemEntity, orderItemsEntity
+from bson import ObjectId # type: ignore
+
 
 order = APIRouter(prefix="/orders", tags=["Orders"])
 
 # Create
 @order.post('')
 def create_order(order_data: Order, itemsIdAndQuantity: dict, userid: str, orderdate: str):
-    for item_id in itemsIdAndQuantity:
-        order_data.items.append(create_order_items(itemsIdAndQuantity[item_id]['productId'], itemsIdAndQuantity[item_id]['quantity'], order_data))
-    #validar el user id
-    if len(userid) != 24:
-        raise HTTPException(status_code=400, detail="Invalid user ID")
-    order_data.userid = userid
-    order_data.orderDate = orderdate
-    order_data.status = "Pending"
-    order_data.totalAmount = sum(item.subtotal for item in order_data.items)
-    order_data.createdAt = datetime.datetime.now()
-    order_data.updatedAt = datetime.datetime.now()
-    result = db.order.insert_one(order_data.dict())
-    return {"order_id": str(result.inserted_id)}
-    
+    return ""
 
-# @order.post('/{order_id}/items')
-def create_order_items(productId: str, quantity: int, order: OrderItem):
-    product_data = db.product.find_one({"_id": productId})
-    if not product_data:
-        raise HTTPException(status_code=404, detail="Product not found")
-    name = product_data['name']
-    price = product_data['price']
-    subtotal = price * quantity
-    order_item = OrderItem(
-        productid=productId,
-        name=name,
-        price=price,
-        quantity=quantity,
-        subtotal=subtotal
-    )
-    return order_item
 
 # Research
 @order.get('')
@@ -57,19 +28,6 @@ def get_orders_by_user(user_id: str):
 def get_order_by_id(order_id: str):
     return {"order_id": order_id}
 
-@order.get('/{order_id}/items')
-def get_order_items_by_id(order_id: str):
-    #validar order id
-    if len(order_id) != 24:
-        raise HTTPException(status_code=400, detail="Invalid order ID")
-    result = orderItemsEntity(db.orderItem.find({"orderid": order_id}))
-    return result
-
-@order.get('/items')
-def get_order_items():
-    return orderItemsEntity(db.orderItem.find())
-
-    
 
 # Update
 @order.put('/{order_id}/status')
