ebe4a0739cbf5f5dc517617db32e64de5fce64dc
Gabriel <afuentes3@ucol.mx>
2025-11-14 14:10:44 -0600

    refactor: reorder router inclusions for improved organization and clarity

--- main.py
+++ main.py
@@ -5,15 +5,17 @@ from routes.store import store
 from routes.user import user
 from routes.category import category
 from routes.order import order
+from routes.orderItem import orderItem
 from routes.payment import payment
 from routes.adress import address
 
 main = FastAPI()
 
-main.include_router(product)
-main.include_router(store)
-main.include_router(payment)
-main.include_router(order)
 main.include_router(user)
-main.include_router(category)
+main.include_router(store)
 main.include_router(address)
+main.include_router(product)
+main.include_router(category)
+main.include_router(order)
+main.include_router(orderItem)
+main.include_router(payment)
