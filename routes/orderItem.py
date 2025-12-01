1481f31080d647d175b5a5dc12ba105561a0d6ae
Gabriel <afuentes3@ucol.mx>
2025-11-30 23:30:58 -0600

    fix: implement deletion of order items and update total amount in order when quantity is zero

--- routes/orderItem.py
+++ routes/orderItem.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from unittest import result
 from config.db import db
 from fastapi import APIRouter, HTTPException
@@ -23,6 +24,10 @@ def create_order_item(productId: str,orderId: str, quantity: int, orderItem_data
     order_item['price'] = product_data['price']
     order_item['subtotal'] = order_item['price'] * quantity
     result = db.orderItem.insert_one(order_item)
+    db.order.update_one(
+        {"_id": ObjectId(orderId)},
+        {"$inc": {"totalAmount": order_item['subtotal']}, "$set": {"updatedAt": datetime.now()}}
+    )
     order_item['id'] = str(result.inserted_id)
     return {"id": str(result.inserted_id)}
 
@@ -46,11 +51,25 @@ def update_order_item_quantity(item_id: str, quantity: int): #Done
     orderItem_data = db.orderItem.find_one({"_id": ObjectId(item_id)})
     if not orderItem_data:
         raise HTTPException(status_code=404, detail="Order item not found")
+    if quantity <= 0:
+        db.orderItem.delete_one({"_id": ObjectId(item_id)})
+        db.order.update_one(
+            {"_id": ObjectId(orderItem_data['orderId'])},
+            {"$inc": {"totalAmount": -orderItem_data['subtotal']}, "$set": {"updatedAt": datetime.now()}}
+        )
+        return {"message": "Order item deleted successfully"}
+
+    pre_subtotal = orderItem_data['subtotal']
     subtotal = orderItem_data['price'] * quantity
     db.orderItem.update_one(
         {"_id": ObjectId(item_id)},
         {"$set": {"quantity": quantity, "subtotal": subtotal}}
     )
+    db.order.update_one(
+        {"_id": ObjectId(orderItem_data['orderId'])},
+        {"$inc": {"totalAmount": subtotal - pre_subtotal}, "$set": {"updatedAt": datetime.now()}}
+    )
+
     return get_order_items_by_id(item_id)
 
 #delete
